a60dc3733a3e8d905a1c4f407e3ead19c5ffaad3
Flávio Codeço Coelho <fccoelho@gmail.com>
2025-08-07 18:03:16 -0300

    removed useless dependencies

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import gradio as gr
 
 from aiwrite.gradgui import app
 
-"""
- For more information on `huggingface_hub` Inference API support, please check the docs: https://huggingface.co/docs/huggingface_hub/v0.22.2/en/guides/inference
-"""
 
 
 if __name__ == "__main__":
