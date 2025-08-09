c276c8a6973ae79c5bb9aab383cb5fc9d60173a0
Flávio Codeço Coelho <fccoelho@gmail.com>
2025-08-09 08:02:02 -0300

    moved dbpath to env variable

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import gradio as gr
 Entry point for the application.
 
 """
+import os
 from aiwrite.gradgui import app
 
 
 
 if __name__ == "__main__":
     # demo.launch()
-    app.main(db_path="/data")
+    app.main(db_path=os.getenv("DB_PATH", "/data"))
